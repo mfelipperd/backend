a1eab41818dcefba0f26e621d0a8d9a65918707e
mfelipperd <felipperabelodurans@gmail.com>
2025-04-02 13:18:22 -0300

    feat: create service

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 import { UpdateEmailDto } from './dto/update-email.dto';
+import { PrismaService } from 'src/prisma.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class EmailsService {
-  create(createEmailDto: CreateEmailDto) {
-    return 'This action adds a new email';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mailer: MailerService,
+  ) {}
+
+  async create(data: CreateEmailDto) {
+    const emailRecord = await this.prisma.emailRecipient.create({ data });
+
+    try {
+      await this.sendConfirmationEmail(emailRecord.email);
+    } catch (error) {
+      console.error('Erro ao enviar e-mail de confirmação de cadastro:', error);
+    }
+
+    return emailRecord;
   }
 
-  findAll() {
-    return `This action returns all emails`;
+  private async sendConfirmationEmail(email: string) {
+    await this.mailer.sendMail({
+      to: email,
+      subject: 'Você foi adicionado para receber notificações',
+      text: `Este e-mail foi configurado para receber notificações de cadastro de novas empresas na plataforma.`,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.emailRecipient.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
   }
 
   findOne(id: number) {
     return `This action returns a #${id} email`;
   }
 
-  update(id: number, updateEmailDto: UpdateEmailDto) {
-    return `This action updates a #${id} email`;
+  async update(id: number, data: UpdateEmailDto) {
+    return this.prisma.emailRecipient.update({
+      where: { id },
+      data,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} email`;
+  async remove(id: number) {
+    return this.prisma.emailRecipient.delete({
+      where: { id },
+    });
   }
 }
